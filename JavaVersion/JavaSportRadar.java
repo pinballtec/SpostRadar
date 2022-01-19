3f7f93e6c1907516372da40ab16a30beb9a87603
pinballtec <daniilboiko99@gmail.com>
2022-01-19 22:38:18 +0100

    output of away_winner and home_winner

--- JavaVersion/JavaSportRadar.java
+++ JavaVersion/JavaSportRadar.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 
 
 public class JavaSportRadar {
@@ -9,9 +12,20 @@ public class JavaSportRadar {
         JSONParser parser= new JSONParser();
         try {
             Object obj = parser.parse(new FileReader("D:\\new_beginning\\SportRadar\\data.json"));
-            JSONObject jsonObject = (JSONObject)obj;
+            JSONObject jsonObject = (JSONObject) obj;
             JSONArray subjects = (JSONArray)jsonObject.get("Events");
-            System.out.println("Events:");
+            for (int i=0; i<subjects.size();i++)
+            {
+                JSONObject Events=(JSONObject)subjects.get(i);
+
+                Double probability_home_team_winner = (Double) Events.get("probability_home_team_winner");
+                Double probability_draw = (Double) Events.get("probability_draw");
+                Double probability_away_team_winner = (Double) Events.get("probability_away_team_winner");
+
+                System.out.println("Home team winner: " + probability_home_team_winner);
+                System.out.println("Away team winner: " + probability_away_team_winner);
+                System.out.println("Draw: " + probability_draw);
+            }
             Iterator iterator = subjects.iterator();
             while (iterator.hasNext()) {
                 System.out.println(iterator.next());
@@ -19,6 +33,8 @@ public class JavaSportRadar {
         } catch(Exception e) {
             e.printStackTrace();
         }
+
+        }
     }
-}
+
 
