27545f38fdbc5aeb5d347965075b850cb7cab95f
pinballtec <daniilboiko99@gmail.com>
2022-01-20 15:13:23 +0100

    visual change

--- pythonVersion.py
+++ pythonVersion.py
@@ -16,13 +16,13 @@ with open(filename, 'r') as f:
                 home_winner = i['probability_home_team_winner']
                 draw = i['probability_draw']
                 if Team_qualifier == 'home':
-                    print(f'{Team_name} is {Team_qualifier}')
+                    print(f'\n{Team_name} is {Team_qualifier}')
                 elif Team_qualifier == 'away':
                     print(f'{Team_name} is {Team_qualifier}')
             if away_winner > home_winner and Team_qualifier == 'away':
-                print(f'The away team will win with: {away_winner} {Team_name}, start date {start_date} on {location}')
+                print(f'The away team will win with: {away_winner}, {Team_name}, start date {start_date} on {location}')
             else:
-                print(f'The home team will win with: {home_winner}, start date {start_date} on {location}')
+                print(f'The home team will win with: {home_winner}, start date {start_date} on {location}\n')
         except TypeError:
             for x in idev:
                 Team_name = x['name']
@@ -31,12 +31,12 @@ with open(filename, 'r') as f:
                 home_winner = i['probability_home_team_winner']
                 draw = i['probability_draw']
                 if Team_qualifier == 'home':
-                    print(f'{Team_name} is {Team_qualifier}')
+                    print(f'\n{Team_name} is {Team_qualifier}')
                 elif Team_qualifier == 'away':
                     print(f'{Team_name} is {Team_qualifier}')
             if away_winner > home_winner and Team_qualifier == 'away':
                 print(f'The away team will win with: {away_winner} {Team_name}, start date {start_date}, no info for location')
             else:
-                print(f'The home team will win with: {home_winner}, start date {start_date}, no info for location')
+                print(f'The home team will win with: {home_winner}, start date {start_date}, no info for location\n')
 
 
