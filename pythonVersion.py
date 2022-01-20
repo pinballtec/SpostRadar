26f825ce3f1bc297d749847b0e54195e55ee1245
pinballtec <daniilboiko99@gmail.com>
2022-01-20 13:20:55 +0100

    location is added

--- pythonVersion.py
+++ pythonVersion.py
@@ -7,17 +7,36 @@ with open(filename, 'r') as f:
         idev = i['competitors']
         start_date = i['start_date']
         venue = i['venue']
-        for x in idev:
-            Team_name = x['name']
-            Team_qualifier = x['qualifier']
-            away_winner = i['probability_away_team_winner']
-            home_winner = i['probability_home_team_winner']
-            draw = i['probability_draw']
-            if Team_qualifier == 'home':
-                print(f'{Team_name} is {Team_qualifier}')
-            elif Team_qualifier == 'away':
-                print(f'{Team_name} is {Team_qualifier}')
-        if away_winner > home_winner and Team_qualifier == 'away':
-            print(f'The away team will win with: {away_winner} {Team_name}, start date {start_date}')
-        else:
-            print(f'The home team will win with: {home_winner}, start date {start_date}')
+        try:
+            location = venue['name']
+            for x in idev:
+                Team_name = x['name']
+                Team_qualifier = x['qualifier']
+                away_winner = i['probability_away_team_winner']
+                home_winner = i['probability_home_team_winner']
+                draw = i['probability_draw']
+                if Team_qualifier == 'home':
+                    print(f'{Team_name} is {Team_qualifier}')
+                elif Team_qualifier == 'away':
+                    print(f'{Team_name} is {Team_qualifier}')
+            if away_winner > home_winner and Team_qualifier == 'away':
+                print(f'The away team will win with: {away_winner} {Team_name}, start date {start_date} on {location}')
+            else:
+                print(f'The home team will win with: {home_winner}, start date {start_date} on {location}')
+        except TypeError:
+            for x in idev:
+                Team_name = x['name']
+                Team_qualifier = x['qualifier']
+                away_winner = i['probability_away_team_winner']
+                home_winner = i['probability_home_team_winner']
+                draw = i['probability_draw']
+                if Team_qualifier == 'home':
+                    print(f'{Team_name} is {Team_qualifier}')
+                elif Team_qualifier == 'away':
+                    print(f'{Team_name} is {Team_qualifier}')
+            if away_winner > home_winner and Team_qualifier == 'away':
+                print(f'The away team will win with: {away_winner} {Team_name}, start date {start_date}, no info for location')
+            else:
+                print(f'The home team will win with: {home_winner}, start date {start_date}, no info for location')
+
+
